02e574a988c51798a6de2ff65b660a3c37f6341a
Juan Rivera <juanriv45@gmail.com>
2018-01-30 04:22:33 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,8 +3,8 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
-  array.each do |names|
-    "Hello, my name is #{names}"
+def batch_badge_creator(names)
+  names.each do |name|
+    "Hello, my name is #{name}"
   end
 end
